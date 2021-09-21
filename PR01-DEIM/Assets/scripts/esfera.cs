6f7982ed746047d7eb59793129da331ca873936a
Alba Muñoz Díaz <chutsi99@gmail.com>
2021-09-21 10:08:25 +0200

    Ej1 a)

---
+++ PR01-DEIM/Assets/scripts/esfera.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class esfera : MonoBehaviour
+{
+    int n = 8;
+    float d = 8f;
+    string nombre;
+    bool up = false;
+    bool down = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        print(n = 7);
+        print(nombre =" Ana ");
+        print(5.5f);
+        print(up);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
