15473245fb05e86c283781f1608276b7cf678d6e
Alba Muñoz Díaz <chutsi99@gmail.com>
2021-09-21 13:29:53 +0200

    Ejercicio 2b

--- PR01-DEIM/Assets/scripts/esfera.cs
+++ PR01-DEIM/Assets/scripts/esfera.cs
@@ -4,25 +4,28 @@ using UnityEngine;
 
 public class esfera : MonoBehaviour
 {
-    int n = 0;
-   
-
+    int aleatorio = 0;
+    [SerializeField] int numElegido;
      // Start is called before the first frame update
     void Start()
     {
-        
+        while (aleatorio != numElegido)
+        {
+            aleatorio = Random.Range(0 , 55);
+            print(aleatorio);
+        }
+
+
+        for (int aleatorio2 = 0; aleatorio2 != numElegido; aleatorio2 = Random.Range(0, 55))
+        {
+            print("Aleatorio FOR" + aleatorio2);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
 
-       if (n <= 100)
-        {
-            print(n);
-            n++;
-           
-        }
 
        
 
